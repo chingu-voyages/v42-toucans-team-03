d73d3f5eb3a8783d9a00c1a614ea30732d8aa144
Quang <76503650+QuangLyHo@users.noreply.github.com>
2023-01-17 18:37:57 -0500

    Feat/23 display fact gong (#24)
    * modified path for background image url
    
    * added an element so I can insert API response
    
    * update to api fetch function and displays fact on index.html
    
    * typo

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const categories = ["animal", "career", "celebrity", "dev", "fashion", "food", "
 
 const url = `https://api.chucknorris.io/jokes/random?category=`
 
-
 function generateRandomNumber() {
     return Math.floor(Math.random() * categories.length - 1) + 1
 }
 
-async function getJoke() {
-    let response = await fetch(url+(categories[generateRandomNumber()]))
+const randomCategory = categories[generateRandomNumber()]
+
+async function getJoke(input = randomCategory) {
+    let response = await fetch(url + input) 
     return await response.json()
 }
 
@@ -16,5 +17,7 @@ getJoke().then(joke => {
     // CN fact destructured, 
     // the value variable contains our CN quote
     const {categories, icon_url, id, url, value} = joke
+    console.log(value)
 
+    document.getElementById('fact').innerHTML = value
 })
