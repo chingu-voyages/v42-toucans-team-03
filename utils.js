7bce6138e743468167389144ce2c1c7efa8fed4d
QuangLyHo <lyhoqq@gmail.com>
2023-02-07 13:15:15 -0500

    Removed utils.js file

--- utils.js
+++
@@ -1,7 +0,0 @@
-// Toggles checkbox to false when screen is larger than 500px
-// So the hamburger menu doesn't stay open
-window.addEventListener('resize', function() {
-    if (window.innerWidth > 500) {
-        document.getElementById("nav-toggle").checked = false;
-    }
-  });
